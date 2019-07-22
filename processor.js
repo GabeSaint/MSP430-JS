7625dd7029308f453a03dd362eb36409258bbc68
manuelaguilarrios1 <51804834+manuelaguilarrios1@users.noreply.github.com>
2019-07-21 21:28:48 -0700

    Update processor.js

--- processor.js
+++ processor.js
@@ -110,10 +110,10 @@ function showRegisters(){
   }
 
   else if(stackPointer == 1){
-    document.getElementById("stackbits0IND").style.display='none';
-    document.getElementById("stackbits1IND").style.display='inline';
-    document.getElementById("stackbits2IND").style.display='none';
-    document.getElementById("stackbits3IND").style.display='none';
+    document.getElementById("stackbits0IND").innerHTML = "undefined";
+    document.getElementById("stackbits1IND").innerHTML = "undefined";
+    document.getElementById("stackbits2IND").innerHTML = "undefined";
+    document.getElementById("stackbits3IND").innerHTML = "undefined";
   }
 
   else if(stackPointer == 2){
